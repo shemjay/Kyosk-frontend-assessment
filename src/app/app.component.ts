cebd27d4cb3413c3ea29f9a57fd2d9686c303598
Shem Jaoko <jaokoshem@gmail.com>
2025-01-30 22:32:09 +0300

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TestComponent } from './components/test/test.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [TestComponent],
+  template: `<app-test />`,
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'shop-app';
